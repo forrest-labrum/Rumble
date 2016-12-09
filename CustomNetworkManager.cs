19f65f34ff586011300d43d5c3b85d97dcba254f
= <=>
2016-12-08 22:15:13 -0700

    add intro network scripts

---
+++ CustomNetworkManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CustomNetworkManager : NetworkManager {
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject proxyPlayer = (GameObject)Instantiate(playerPrefab, playerPrefab.transform.position, playerPrefab.transform.rotation);
+        NetworkServer.AddPlayerForConnection(conn, proxyPlayer, playerControllerId);
+    }
+}
